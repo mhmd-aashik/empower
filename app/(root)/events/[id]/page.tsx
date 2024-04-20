880046aa0ecdf977538f092725753712bd41a30a
mhmd-aashik <aashikdevelop@gmail.com>
2024-04-20 21:41:55 +0530

    one time

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react'
 
-const page = ({ params }: { params: { id: string } }) => {
-//   return <div>
-//     <Image src={}
-//   </div>;
-};
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
 
-export default page;
+export default page
