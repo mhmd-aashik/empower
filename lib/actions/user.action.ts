ca98fe994d271301b523104ab0fe37efd44cd91f
mhmd-aashik <aashikdevelop@gmail.com>
2024-04-20 21:26:10 +0530

    sdfghjkl

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -15,30 +15,30 @@ export async function createUser(user: CreateUserParams) {
   }
 }
 
-export async function getUserById(userId: string) {
-  try {
-    await connectToDatabase();
+// export async function getUserById(userId: string) {
+//   try {
+//     await connectToDatabase();
 
-    const user = await User.findById(userId);
+//     const user = await User.findById(userId);
 
-    if (!user) throw new Error("User not found");
-    return JSON.parse(JSON.stringify(user));
-  } catch (error) {
-    console.log(error);
-  }
-}
+//     if (!user) throw new Error("User not found");
+//     return JSON.parse(JSON.stringify(user));
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-export async function updateUser(clerkId: string, user: UpdateUserParams) {
-  try {
-    await connectToDatabase();
+// export async function updateUser(clerkId: string, user: UpdateUserParams) {
+//   try {
+//     await connectToDatabase();
 
-    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
-      new: true,
-    });
+//     const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
+//       new: true,
+//     });
 
-    if (!updatedUser) throw new Error("User update failed");
-    return JSON.parse(JSON.stringify(updatedUser));
-  } catch (error) {
-    console.log(error);
-  }
-}
+//     if (!updatedUser) throw new Error("User update failed");
+//     return JSON.parse(JSON.stringify(updatedUser));
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
